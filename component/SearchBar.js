d88972b1b43eefcac3594aa6cb002a8aad19b6e9
aniketgarje <aniket.garje@provistechnologies.com>
2023-05-08 19:15:02 +0530

    changes

--- component/SearchBar.js
+++ component/SearchBar.js
@@ -1,5 +1,5 @@
 import { BsArrowRightShort } from "react-icons/bs";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectSearchChat, searchFetch, setSearchChat } from "@/store/slices/searchSlice";
 import { useRouter } from "next/router";
@@ -11,7 +11,7 @@ const SearchBar = ({ placeholder, isLoading, setIsLoading }) => {
   const router = useRouter();
   const searchChat = useSelector(selectSearchChat);
   const textareaRef = useRef(null);
-
+  const [page,setPage] = useState(false);
   const handleSearch = () => {
     if (searchText === '') {
       textareaRef.current.focus();
@@ -28,7 +28,12 @@ const SearchBar = ({ placeholder, isLoading, setIsLoading }) => {
       }
     });
   };
-
+  
+  useEffect(()=>{
+    if(router.pathname.startsWith('/search')){
+   setPage(true)
+    }
+  },[])
   const handletextareaOnchange = (e) => {
     let elem = document.getElementById("home-textarea");
     elem.style.height = `${elem.scrollHeight}px`;
@@ -36,15 +41,22 @@ const SearchBar = ({ placeholder, isLoading, setIsLoading }) => {
   };
 
   const placeholderinput = "iPhone 13, 128GB, Product Red - Unlocked (Renewed Premium)"
-  return <div className="position-relative">
+  return (
+
+  <div className="position-relative">
     {/* <textarea id='home-textarea' ref={textareaRef} className="w-100 search-input-home border-0" placeholder={placeholder} value={searchText} onChange={(e) => handletextareaOnchange(e)}></textarea>
     <div className="text-end">
       <button onClick={(e) => handleSearch(e)} disabled={isLoading} className="search-button btn rounded-0">{!isLoading ? 'Search' : 'Loading...'} <BsArrowRightShort className='right-arrow' /></button>
     </div> */}
-    
-    <input ref={textareaRef} id='input-1' className='w-100 search-input-homepage' placeholder={placeholderinput}/>
-    <span className="search-icon-span"><CiSearch className="search-icon"/></span>
-    </div>
+      {page ? 
+      <div className="w-100">
+      <input ref={textareaRef} id='home-textarea' className='w-80 search-input-homepage' placeholder='Type your Message' value={searchText} onChange={(e) => handletextareaOnchange(e)} />
+      <button className="btn ">Send</button>
+      </div>
+      :<div>
+    <input ref={textareaRef} id='home-textarea' className='w-100 search-input-homepage' placeholder={placeholderinput} value={searchText} onChange={(e) => handletextareaOnchange(e)} />
+    <span className="search-icon-span" onClick={(e) => handleSearch(e)} disabled={isLoading}><CiSearch className="search-icon"/></span>
+ </div> }</div>)
 };
 
 export default SearchBar;

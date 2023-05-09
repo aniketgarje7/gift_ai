354d38b8c44f1f71cbf5a35327f1f8a1997a1058
aniketgarje <aniket.garje@provistechnologies.com>
2023-05-09 18:51:24 +0530

    slider fixes

--- component/Products.js
+++ component/Products.js
@@ -5,6 +5,7 @@ import { selectResult, selectSearchChat } from "@/store/slices/searchSlice";
 
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import { CiMobile2 } from "react-icons/ci";
 
 const Products = () => {
 
@@ -25,10 +26,14 @@ const Products = () => {
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 3
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 614, min: 400 },
+      items: 2
+    },
+      C: {
+      breakpoint: { max: 400, min: 0 },
       items: 1
     }
   };
@@ -41,7 +46,7 @@ const Products = () => {
           </div>
           <div className="col-md-10 mx-auto cursor-pointer">
             <div className="row  justify-content-center">
-              <Carousel responsive={responsive} showDots={true} removeArrowOnDeviceType={["tablet", "mobile", "desktop"]} dotListClass="custom-dot-list-style">
+              <Carousel responsive={responsive} showDots={true} removeArrowOnDeviceType={["tablet", "mobile", "desktop","C"]} dotListClass="custom-dot-list-style">
               {result.products.map((product, key) => 
                 <div className="rounded p-1 m-1 product-card " key={key+'12'}>
                     <div className="card border-0">
